44078af03d479613e86efcc35bb014b80ee238eb
Aakash Kumar <72309525+Aakashsingh2110@users.noreply.github.com>
2022-12-15 13:29:49 +0530

    Create Instance_of_keyword.java

---
+++ Instance_of_keyword.java
@@ -0,0 +1,17 @@
+public static void main(String[] args) {
+
+Object str = new String(“abc”);
+
+If(str instanceof String) {
+
+System.out.println(“String value:” +str);
+
+}
+
+If(str instanceof Integer) {
+
+   System.out.println(“Integer value:” +str);
+
+}
+
+}
